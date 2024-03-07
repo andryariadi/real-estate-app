c432d3b87d5ffcdc46bb5f5c078166c795f910ac
Andry Ariadi <andryariadi23@gmail.com>
2024-03-07 16:49:32 +0700

    styling sidebar

--- estate-app/src/components/navbar/Navbar.jsx
+++ estate-app/src/components/navbar/Navbar.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import "./navbar.scss";
 
 export default function Navbar() {
+  const [open, setOpen] = useState(false);
+
   return (
     <>
       <nav>
@@ -22,10 +25,35 @@ export default function Navbar() {
           </a>
         </div>
         <div className="right">
-          <a href="/">Sign In</a>
+          <a href="/" className="login">
+            Sign In
+          </a>
           <a href="/" className="register">
             Sign Up
           </a>
+          <div className="menuIcon">
+            <img src="/menu.png" alt="menu" onClick={() => setOpen(!open)} />
+          </div>
+          <div className={open ? "menu active" : "menu"}>
+            <a className="nav-link" href="/">
+              Home
+            </a>
+            <a className="nav-link" href="/">
+              About
+            </a>
+            <a className="nav-link" href="/">
+              Contact
+            </a>
+            <a className="nav-link" href="/">
+              Agents
+            </a>
+            <a className="nav-link" href="/">
+              Sign In
+            </a>
+            <a className="nav-link" href="/">
+              Sign Up
+            </a>
+          </div>
         </div>
       </nav>
     </>
