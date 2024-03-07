49d58d259b9d035b8f66593f92efd1602f9645b6
Andry Ariadi <andryariadi23@gmail.com>
2024-03-07 13:30:02 +0700

    setup react vite

---
+++ estate-app/src/App.jsx
@@ -0,0 +1,13 @@
+import "./layout.scss";
+
+function App() {
+  return (
+    <>
+      <main className="layout">
+        <div>Yoo bre..</div>
+      </main>
+    </>
+  );
+}
+
+export default App;
