71f3590e4736157b75f3a697d870c63b8f256d22
Andry Ariadi <andryariadi23@gmail.com>
2024-03-19 16:18:00 +0700

    add filter component

--- estate-app/src/routes/ListPage/ListPage.jsx
+++ estate-app/src/routes/ListPage/ListPage.jsx
@@ -1,7 +1,18 @@
+import "./listPage.scss";
+import { listData } from "../../libs/dummydata";
+
 export default function ListPage() {
+  const data = listData;
+
+  console.log(data, "<----dilistpage");
   return (
     <>
-      <div>ListPage</div>
+      <div className="container">
+        <div className="listContainer">
+          <div className="wrapper">wrapper</div>
+        </div>
+        <div className="mapContainer">Map</div>
+      </div>
     </>
   );
 }
