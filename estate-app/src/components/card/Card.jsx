684b5e6279284b1ddb6af14b633f056b81a8ddfb
Andry Ariadi <andryariadi23@gmail.com>
2024-03-20 16:31:58 +0700

    styling card component

--- estate-app/src/components/card/Card.jsx
+++ estate-app/src/components/card/Card.jsx
@@ -1,9 +1,15 @@
 import "./card.scss";
+import { Link } from "react-router-dom";
 export default function Card({ item }) {
   console.log(item, "<----dicard");
   return (
     <>
-      <div className="container">Card Comp</div>
+      <div className="card">
+        <Link to={`/${item.id}`} className="imgContainer">
+          <img src={item.img} alt={item.title} />
+        </Link>
+        <div className="textContainer"></div>
+      </div>
     </>
   );
 }
