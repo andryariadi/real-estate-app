bf28362f2fdd888a547d530d0bf59e42a2ff21ae
Andry Ariadi <andryariadi23@gmail.com>
2024-03-23 16:18:24 +0700

    styling map compoenent

--- estate-app/src/components/map/Map.jsx
+++ estate-app/src/components/map/Map.jsx
@@ -1,17 +1,16 @@
 import "./map.scss";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import Pin from "../pin/Pin";
 
 export default function Map({ items }) {
   return (
     <>
       <MapContainer center={[52.4797, -1.90269]} zoom={7} scrollWheelZoom={false} className="map">
         <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-        <Marker position={[52.4797, -1.90269]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        {items.map((item) => (
+          <Pin key={item.id} item={item} />
+        ))}
       </MapContainer>
     </>
   );
