9f92d9a7f328d1a50bdc366b353e9fbc2f7092e8
Andry Ariadi <andryariadi23@gmail.com>
2024-03-24 15:49:52 +0700

    add detail page

--- estate-app/src/App.jsx
+++ estate-app/src/App.jsx
@@ -3,6 +3,7 @@ import HomePage from "./routes/HomePage/HomePage";
 import { createBrowserRouter, RouterProvider, Route, Link } from "react-router-dom";
 import Layout from "./routes/layout/layout";
 import ListPage from "./routes/ListPage/ListPage";
+import DetailPage from "./routes/SinglePage/DetailPage";
 
 function App() {
   const router = createBrowserRouter([
@@ -17,6 +18,10 @@ function App() {
         {
           path: "/list",
           element: <ListPage />,
+        },
+        {
+          path: "/:id",
+          element: <DetailPage />,
         },
       ],
     },
---
+++ estate-app/src/routes/SinglePage/DetailPage.jsx
@@ -0,0 +1,16 @@
+import "./detailpage.scss";
+
+export default function DetailPage() {
+  return (
+    <>
+      <div className="detailContainer">
+        <div className="details">
+          <div className="wrapper">Andry</div>
+        </div>
+        <div className="features">
+          <div className="wrapper">Ariadi</div>
+        </div>
+      </div>
+    </>
+  );
+}
