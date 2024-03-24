378817ca100fe1a1f94765dff17da1cdf3f3e5da
Andry Ariadi <andryariadi23@gmail.com>
2024-03-24 16:19:37 +0700

    styling single page

--- estate-app/src/routes/SinglePage/DetailPage.jsx
+++ estate-app/src/routes/SinglePage/DetailPage.jsx
@@ -1,16 +1,34 @@
 import Slider from "../../components/slider/Slider";
+import { singlePostData, userData } from "../../libs/dummydata";
 import "./detailpage.scss";
 
 export default function DetailPage() {
+  const detailData = singlePostData;
+
+  console.log(detailData, "<----disinglepage");
+
   return (
     <>
       <div className="detailContainer">
         <div className="details">
           <div className="wrapper">
-            <Slider />
+            <Slider images={detailData.images} />
             <div className="infoContainer">
-              <div className="top">Top</div>
-              <div className="bottom">Bottom</div>
+              <div className="top">
+                <div className="post">
+                  <h1>{detailData.title}</h1>
+                  <div className="address">
+                    <img src="/pin.png" alt={detailData.title} />
+                    <span>{detailData.address}</span>
+                  </div>
+                  <div className="price">$ {detailData.price}</div>
+                </div>
+                <div className="user">
+                  <img src={userData.img} alt={userData.name} />
+                  <span>{userData.name}</span>
+                </div>
+              </div>
+              <div className="bottom">{detailData.description}</div>
             </div>
           </div>
         </div>
