281caddc6d66701afa5567ef403d38df6c8b6e80
Andry Ariadi <andryariadi23@gmail.com>
2024-03-24 15:55:13 +0700

    add slider component

---
+++ estate-app/src/components/slider/Slider.jsx
@@ -0,0 +1,9 @@
+import "./slider.scss";
+
+export default function Slider() {
+  return (
+    <>
+      <div className="slider">Slider Comp</div>
+    </>
+  );
+}
--- estate-app/src/routes/SinglePage/DetailPage.jsx
+++ estate-app/src/routes/SinglePage/DetailPage.jsx
@@ -1,3 +1,4 @@
+import Slider from "../../components/slider/Slider";
 import "./detailpage.scss";
 
 export default function DetailPage() {
@@ -5,7 +6,13 @@ export default function DetailPage() {
     <>
       <div className="detailContainer">
         <div className="details">
-          <div className="wrapper">Andry</div>
+          <div className="wrapper">
+            <Slider />
+            <div className="infoContainer">
+              <div className="top">Top</div>
+              <div className="bottom">Bottom</div>
+            </div>
+          </div>
         </div>
         <div className="features">
           <div className="wrapper">Ariadi</div>
