2da0480db0110698eadb68a9562e093b345c6ce8
Andry Ariadi <andryariadi23@gmail.com>
2024-03-25 16:28:45 +0700

    styling slider component

--- estate-app/src/components/slider/Slider.jsx
+++ estate-app/src/components/slider/Slider.jsx
@@ -1,9 +1,37 @@
+import { useState } from "react";
 import "./slider.scss";
 
-export default function Slider() {
+export default function Slider({ images }) {
+  const [imgIndex, setImgIndex] = useState(null);
+
   return (
     <>
-      <div className="slider">Slider Comp</div>
+      <div className="slider">
+        {imgIndex !== null && (
+          <div className="fullSlider">
+            <div className="arrow">
+              <img src="/arrow.png" alt="" />
+            </div>
+            <div className="imgContainer">
+              <img src={images[imgIndex]} alt="" />
+            </div>
+            <div className="arrow">
+              <img src="/arrow.png" alt="" className="rightArrow" />
+            </div>
+            <div className="close" onClick={() => setImgIndex(null)}>
+              X
+            </div>
+          </div>
+        )}
+        <div className="bigImage">
+          <img src={images[0]} alt="" onClick={() => setImgIndex(0)} />
+        </div>
+        <div className="smallImage">
+          {images.slice(1).map((image, index) => (
+            <img key={index} src={image} alt="" onClick={() => setImgIndex(index + 1)} />
+          ))}
+        </div>
+      </div>
     </>
   );
 }
