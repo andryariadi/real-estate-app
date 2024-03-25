85b7bca65d57d4903fffaf99755a01a3467d69d1
Andry Ariadi <andryariadi23@gmail.com>
2024-03-25 16:39:48 +0700

    setup slider image

--- estate-app/src/components/slider/Slider.jsx
+++ estate-app/src/components/slider/Slider.jsx
@@ -4,19 +4,35 @@ import "./slider.scss";
 export default function Slider({ images }) {
   const [imgIndex, setImgIndex] = useState(null);
 
+  const changeSlider = (direction) => {
+    if (direction === "left") {
+      if (imgIndex === 0) {
+        setImgIndex(images.length - 1);
+      } else {
+        setImgIndex(imgIndex - 1);
+      }
+    } else {
+      if (imgIndex === images.length - 1) {
+        setImgIndex(0);
+      } else {
+        setImgIndex(imgIndex + 1);
+      }
+    }
+  };
+
   return (
     <>
       <div className="slider">
         {imgIndex !== null && (
           <div className="fullSlider">
             <div className="arrow">
-              <img src="/arrow.png" alt="" />
+              <img src="/arrow.png" alt="" onClick={() => changeSlider("left")} />
             </div>
             <div className="imgContainer">
               <img src={images[imgIndex]} alt="" />
             </div>
             <div className="arrow">
-              <img src="/arrow.png" alt="" className="rightArrow" />
+              <img src="/arrow.png" alt="" className="rightArrow" onClick={() => changeSlider("right")} />
             </div>
             <div className="close" onClick={() => setImgIndex(null)}>
               X
